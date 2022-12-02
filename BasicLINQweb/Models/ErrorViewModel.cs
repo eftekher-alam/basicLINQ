901eba390930fffc0a2f566309c4ebda740c2377
eftekher-alam <eftekher@yahoo.com>
2022-12-02 21:44:26 +0600

    Initial commit NuGet Package added

---
+++ BasicLINQweb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BasicLINQweb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
